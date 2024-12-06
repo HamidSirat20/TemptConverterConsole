b5c74f4b96937a039a7a3547dabde9fa1b79b8b7
HamidSirat20 <abeshaqzada@gmail.com>
2024-12-06 23:24:56 +0200

    Created

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int value = 0;
+        string? unit = null;
+        bool isValid = false;
+
+        while (!isValid)
+        {
+            Console.Write("Please enter a value and unit as {value C/F}: ");
+            var userInput = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userInput) || !userInput.Contains(' '))
+            {
+                Console.WriteLine("Invalid format. Try again.");
+                continue;
+            }
+
+            var valueAndUnit = userInput.Split(' ');
+
+            if (valueAndUnit.Length == 2 &&
+                int.TryParse(valueAndUnit[0], out value) &&
+                (valueAndUnit[1].Equals("C", StringComparison.OrdinalIgnoreCase) ||
+                 valueAndUnit[1].Equals("F", StringComparison.OrdinalIgnoreCase)))
+            {
+                unit = valueAndUnit[1].ToLower();
+                isValid = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid format. Try again.");
+            }
+
+            TempConvert(unit, value);
+        }
+
+
+
+        static void TempConvert(string unit, int value)
+        {
+            switch (unit)
+            {
+                case "c":
+                    double fahrenheit = (9.0 / 5.0) * value + 32;
+                    Console.WriteLine($"Your value in Fahrenheit is {fahrenheit:F2} F.");
+                    break;
+
+                case "f":
+                    double celsius = (value - 32.0) * (5.0 / 9.0);
+                    Console.WriteLine($"Your value in Celsius is {celsius:F3} C.");
+                    break;
+
+                default:
+                    Console.WriteLine("No such unit defined.");
+                    break;
+            }
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+## Temperature Converter
+You are tasked to develop a temperature converter program in C#. The program will accept temperature values in Fahrenheit, Celsius, and convert them to their corresponding values in the other units.
+
+## Requirements
+Prompt the user to input a temperature value and a temperature scale (either Celsius or Fahrenheit).
+Convert the temperature to the opposite scale and display the result.
+Handle invalid user inputs by displaying an error message and prompting the user to enter valid inputs.
+Write a function called TempConvert to perform the temperature conversions.
+Use switch statement to determine which conversion function to use based on the user's input for the temperature scale.
